bb050680e9bbf4f88ccf47bd5d0077468cc90f28
uhyunjin <uhyeonjin0619@gmail.com>
2024-03-24 23:55:41 +0900

    namespace

--- src/main/java/com/fastcampus/ch4/dao/BoardDao.java
+++ src/main/java/com/fastcampus/ch4/dao/BoardDao.java
@@ -10,7 +10,9 @@ public class BoardDao {
     @Autowired
     SqlSession session;
 
+    String namespace="com.fastcampus.ch4.dao.BoardMapper.";
+
     public BoardDto select(int bno) throws Exception {
-        return session.selectOne("select", bno);
+        return session.selectOne(namespace+"select", bno);
     }
 }
