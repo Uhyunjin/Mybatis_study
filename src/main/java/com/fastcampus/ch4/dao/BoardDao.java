9bf7c4d4934be04f506ce7dc5ddab4047ecafeb1
uhyunjin <uhyeonjin0619@gmail.com>
2024-03-25 00:00:46 +0900

    interface 추출

--- src/main/java/com/fastcampus/ch4/dao/BoardDao.java
+++ src/main/java/com/fastcampus/ch4/dao/BoardDao.java
@@ -1,18 +1,7 @@
 package com.fastcampus.ch4.dao;
 
 import com.fastcampus.ch4.domain.BoardDto;
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 
-public class BoardDao {
-
-    @Autowired
-    SqlSession session;
-
-    String namespace="com.fastcampus.ch4.dao.BoardMapper.";
-
-    public BoardDto select(int bno) throws Exception {
-        return session.selectOne(namespace+"select", bno);
-    }
+public interface BoardDao {
+    BoardDto select(int bno) throws Exception;
 }
