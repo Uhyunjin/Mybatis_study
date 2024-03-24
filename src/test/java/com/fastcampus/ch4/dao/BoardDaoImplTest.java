698c5a11e4ac6e9b574b25924d3462470de7d5f7
uhyunjin <uhyeonjin0619@gmail.com>
2024-03-25 00:21:33 +0900

    board dto에 담아서 가져오기 test

--- src/test/java/com/fastcampus/ch4/dao/BoardDaoImplTest.java
+++ src/test/java/com/fastcampus/ch4/dao/BoardDaoImplTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.ch4.dao;
 
+import com.fastcampus.ch4.domain.BoardDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,11 @@ public class BoardDaoImplTest {
     BoardDao boardDao;
 
     @Test
-    public void select() {
+    public void select() throws Exception{
         assertTrue(boardDao!=null);
         System.out.println("boardDao = " + boardDao);
+        BoardDto boardDto = boardDao.select(1);
+        System.out.println("boardDto = " + boardDto);
+        assertTrue(boardDto.getBno().equals(1));
     }
 }
