1cbb85220ba0c6a8b6c969a2967285dcf1c0ae0e
uhyunjin <uhyeonjin0619@gmail.com>
2024-03-25 00:14:51 +0900

    board dao test(junit4)

--- src/main/java/com/fastcampus/ch4/dao/BoardDaoImpl.java
+++ src/main/java/com/fastcampus/ch4/dao/BoardDaoImpl.java
@@ -3,7 +3,9 @@ package com.fastcampus.ch4.dao;
 import com.fastcampus.ch4.domain.BoardDto;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class BoardDaoImpl implements BoardDao {
 
     @Autowired
